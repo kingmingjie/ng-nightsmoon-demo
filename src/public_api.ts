b47e88c40a69260dcfbe21e3ab6203e956943670
NightsMoon <37955372+kingmingjie@users.noreply.github.com>
2019-11-05 17:03:22 +0800

    commit files

---
+++ src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-nightsmoon-demo
+ */
+
+export * from './lib/ng-nightsmoon-demo.service';
+export * from './lib/ng-nightsmoon-demo.component';
+export * from './lib/ng-nightsmoon-demo.module';
